4bc21c6d0b5dead207f97dfb3f1f55ada7f4ea3d
Janusz Spatz <janusz.spatz@mercedes-benz.com>
2023-09-29 14:21:32 +0000

    try catch guard

--- src/baldr/graphtile.cc
+++ src/baldr/graphtile.cc
@@ -369,8 +369,11 @@ void GraphTile::Initialize(const GraphId& graphid) {
     lane_connectivity_size_ = header_->end_offset() - header_->lane_connectivity_offset();
   }
 
-  live_speed_fading_sec_ = config().get<float>("sif.live_speed_fading_sec", 3600);
-
+  try {
+    live_speed_fading_sec_ = config().get<float>("sif.live_speed_fading_sec", 3600);
+  } catch (const ConfigUninitializedException &e){
+    live_speed_fading_sec_ = 3600;
+  }
   // For reference - how to use the end offset to set size of an object (that
   // is not fixed size and count).
   // example_size_ = header_->end_offset() - header_->example_offset();
